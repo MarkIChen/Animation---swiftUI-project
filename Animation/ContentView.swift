6e9bc42af0b888a72322041a4b308033e6b62481
MarkStudio <markchen8024@gmail.com>
2020-08-07 15:35:09 +0800

    final.

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,47 +8,24 @@
 
 import SwiftUI
 
-struct CornerRotateModifier: ViewModifier {
-    let amount: Double
-    let anchor: UnitPoint
-    
-    func body(content: Content) -> some View {
-        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
-    }
+class User: ObservableObject {
+    @Published var name = ""
+    @Published var password = ""
 }
-
-extension AnyTransition {
-    static var pivot: AnyTransition {
-        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading)
-            , identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
-    }
-}
-
 
 struct ContentView: View {
-    @State private var showRec = false
-    
+    @ObservedObject private var user =  User()
     
     var body: some View {
         VStack {
-            Button("Tap me") {
-                withAnimation {
-                    self.showRec.toggle()
-                }
-            }
+            Text("naem: \(self.user.name), password: \(user.password)")
             
-            if showRec {
-                
-                Rectangle()
-                    .frame(width: 200, height: 200, alignment: .center)
-                   
-//                    .transition(.asymmetric(insertion: .slide, removal: .opacity))
-                    .transition(.pivot)
-                 .background(LinearGradient(gradient: Gradient(colors: [.red, .yellow]), startPoint: .topLeading, endPoint: .bottomLeading))
-            }
-            
+            TextField("user name", text: $user.name)
+            TextField("password", text: $user.password)
         }
+    
     }
+       
 }
 
 struct ContentView_Previews: PreviewProvider {
